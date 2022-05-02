28a616517d1dbaa02f1d41a7f29ce409f275c793
Snackbar <56302911+asnackbar@users.noreply.github.com>
2022-05-02 22:10:39 +0200

    auto changes

--- auto.sh
+++ auto.sh
@@ -6,8 +6,9 @@
 while true; do
 
   inotifywait -qq -e CLOSE_WRITE 1.8.2server
+  inotifywait -qq -e CLOSE_WRITE plans
 
-  cd 1.8.2server; git commit -a -m 'autocommit on change'
+  git commit -a -m 'autocommit on change'
   git push
 
 done
