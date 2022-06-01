ac5a78a260589eb89eca7c1ee5299cceb2b970be
Snackbar <56302911+asnackbar@users.noreply.github.com>
2022-06-02 01:06:33 +0200

    leak
    remmove leaked documents

--- auto.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#
-# gitwait - watch file and git commit all changes as they happen
-#
-
-<<branch>> = 
-<<file>> = 
-inotifywait -q -m -e CLOSE_WRITE --format="git commit -m 'auto commit' %w && git push origin https://github.com/asnackbar/Vanillaserver.git" auto.sh | bash
--- auto_commit.sh
+++
@@ -1,7 +0,0 @@
-
-#!/bin/bash
-MESSAGE="Auto-commit: $(date)"
-REPO_PATH="Vanillaserver"
-git -C "$REPO_PATH" add -A
-git -C "$REPO_PATH" commit -m "$MESSAGE"
-git -C "$REPO_PATH" push 
